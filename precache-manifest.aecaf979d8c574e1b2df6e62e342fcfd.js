2087ae919319e47bf9d403b8a9de205bf38a4508
websudo <ritiknair19@gmail.com>
2020-07-16 12:00:11 +0530

    Updates

---
+++ precache-manifest.aecaf979d8c574e1b2df6e62e342fcfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ec1c8c000150c9a991f88e8531af41",
+    "url": "/foreveryours/index.html"
+  },
+  {
+    "revision": "ad84f81df25588decde8",
+    "url": "/foreveryours/static/css/main.8d13c267.chunk.css"
+  },
+  {
+    "revision": "7faa3381ab7604d4d74d",
+    "url": "/foreveryours/static/js/2.0a1ff533.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foreveryours/static/js/2.0a1ff533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad84f81df25588decde8",
+    "url": "/foreveryours/static/js/main.34e67fd3.chunk.js"
+  },
+  {
+    "revision": "a0eabf2eae650d30c8f1",
+    "url": "/foreveryours/static/js/runtime-main.4e0fc929.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/foreveryours/static/media/logo.5d5d9eef.svg"
+  }
+]);
